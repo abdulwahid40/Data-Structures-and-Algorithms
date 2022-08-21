cdf90fda67fbdc4389baf1dbf9c4672ec4f154af
Abdul Wahid <abdul.wahid1257@gmail.com>
2022-08-21 14:35:43 +0500

    Add files via upload

--- in_Python/binary_search_iteratively.py
+++ in_Python/binary_search_iteratively.py
@@ -1,18 +1,19 @@
 def binary_search_iteratively(A, key):
     left_index = 0
-    right_index = len(A)
+    right_index = len(A) - 1
 
     while left_index <= right_index:
         mid_point = (left_index + right_index) // 2
+        print(mid_point)
         if key == A[mid_point]:
             return mid_point
         elif key < A[mid_point]:
             right_index = mid_point - 1
         elif key > A[mid_point]:
-            left_index = mid_point - 1
+            left_index = mid_point + 1
     
     return -1
 
 A = [15, 21, 47, 84, 96]
-found = binary_search_iteratively(A, 84)
+found = binary_search_iteratively(A, 21)
 print(f'Result: {found}')
